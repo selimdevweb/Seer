2f14cf1da78a693cdcacc0531a70eeaa6b401d33
Cedric Marchal <cmarchal1999@gmail.com>
2021-05-25 20:19:21 +0200

    t

--- resources/views/user_auth/dashboard.blade.php
+++ resources/views/user_auth/dashboard.blade.php
@@ -71,7 +71,7 @@
     @elseif (auth()->user()->status ==1)
 
 
-        <div class="d-flex flex-column justify-content-center align-items-center">
+<div class="d-flex flex-column justify-content-center align-items-center">
         <article class="user_card user_dashboard">
             <article class="alert alert-primary text-center ">
                 <span>Vos Documents sont approuvés !</span>
@@ -79,12 +79,26 @@
                         <a class="m-2" href="{{ asset('pdf/'.$file->file_path) }} " target="blank">Voir pdf</a>
                 @endforeach
             </article>
+
             @foreach ($billeteries as $billetterie)
-                <div class="text-center">
-                    <h6>Distribution du {{ \Carbon\Carbon::parse($billetterie->date)->translatedFormat('d/m/Y') }}</h6>
-                    <span>Prix : {{ $billetterie->prix }}€</span>
-                    <p>Quantité(s) : {{ $billetterie->quantite }}</p>
-                    <a href="{{ route('add.cart', $billetterie->id) }}">Ajouter au panier</a>
+                <div class="d-flex justify-content-center align-items-center text-center billetterie user_card" style="height: 500px">
+                    <div>
+                        <img src="{{ asset('images/colis-seer.jpg') }}" >
+                    </div>
+                    <div class="product_desc">{{--
+                        <h6 class="txt">Distribution du {{ \Carbon\Carbon::parse($billetterie->date)->translatedFormat('d/m/Y') }} --}}</h6>
+                        <h2>Colis secs - {{ \Carbon\Carbon::parse($billetterie->date)->translatedFormat('d/m/Y') }}</h2>
+                        <h3>{{ $billetterie->prix }}€</h3>
+                        {{-- <p>Quantité(s) : {{ $billetterie->quantite }}</p> --}}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lacinia bibendum massa, in dignissim dui blandit nec. Morbi lobortis elit mollis efficitur consequat. Etiam varius sem eu tempor feugiat. Vestibulum convallis tortor sed ex blandit, eget cursus nibh tempor. Duis laoreet tincidunt sem, in consectetur ligula facilisis ut.</p>
+                        <div class="product"><span>Disponibilité: En stock</span></div>
+                        <form action="{{ route('cart.update', $billetterie->id) }}" class="user_shop">
+                            @csrf
+                            <input type="number" name="quantity" value="1">
+                            <input type="submit" value="Ajouter au panier">
+                        </form>
+                        {{-- <a href="{{ route('add.cart', $billetterie->id) }}">Ajouter au panier</a> --}}
+                    </div>
                 </div>
             @endforeach
         </article>
