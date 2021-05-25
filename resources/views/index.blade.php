4b961d22170d8614854cfd98addc48ffc0405c1d
selimdevweb <selim-kune-do@hotmail.fr>
2021-05-25 14:53:20 +0200

    master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,7 @@
 
 @section('content')
 <div class="d-flex justify-content-center">
-<<<<<<< HEAD
     <h1>Accueil SEER</h1>
-=======
-<h1>Accueil SEER</h1>
->>>>>>> 37bc57b7885d52fe7d674aa8cc50ca6defaa3849
     @if (session()->has('message'))
     <div class="w-4/5 m-auto mt-10 pl-2">
         <p class="w2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl px-4 py-4">
