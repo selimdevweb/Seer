1637e357a82067dc54dc7045284c632c93dff85c
Cedric Marchal <cmarchal1999@gmail.com>
2021-05-31 21:28:49 +0200

    checkout

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -8,34 +8,36 @@
 
     <div class="main__content">
         <h1>Votre panier</h1>
-        <table>
-            <thead></thead>
-                <tr>
-                    <th>Produit(s)</th>
-                    <th>Prix</th>
-                    <th>Quantité(s)</th>
 
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($billetteries as $billetterie)
-                <tr>
-                    <td scope="row">{{ $billetterie->name }}</td>
-                    <td scope="row">{{ \Cart::get($billetterie->id)->getPriceSum()}} €</td>
-                    <td scope="row">
-                        <div class="main__form">
-                            <form action="{{ route('cart.update', $billetterie->id) }}">
-                                @csrf
+        <div class="main__tableau">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Produit(s)</th>
+                        <th>Prix</th>
+                        <th>Quantité(s)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($billetteries as $billetterie)
+                    <tr>
+                        <td scope="row">{{ $billetterie->name }}</td>
+                        <td scope="row">{{ \Cart::get($billetterie->id)->getPriceSum()}} €</td>
+                        <td scope="row">
+                            <div class="main__form">
+                                <form action="{{ route('cart.update', $billetterie->id) }}">
+                                    @csrf
 
-                                <input type="number" name="quantity" value="{{ $billetterie->quantity }}">
-                                <button type="submit">Commander</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                                    <input type="number" name="quantity" value="{{ $billetterie->quantity }}">
+                                    <button type="submit">Commander</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
 
