9b9077cc598dd4130c4fb7e2cc1d76b79040bc4e
Cedric Marchal <cmarchal1999@gmail.com>
2021-05-31 16:13:14 +0200

    test

--- resources/views/user_auth/login.blade.php
+++ resources/views/user_auth/login.blade.php
@@ -12,29 +12,25 @@
             <form action="{{ route('login.store') }}" method="POST">
                 @csrf
 
-                <div>
                     <div class="mb-3 ">
                         <input type="email" name="email" placeholder="Votre adresse mail" value="{{ old('email') }}">
+
+                        @error('email')
+                            <div class="alert alert-danger" role="alert">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
 
-                    @error('email')
-                        <div class="alert alert-danger" role="alert">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
                     <div class="mb-3">
                         <input type="password" name="password" placeholder="Votre mot de passe">
-                    </div>
 
-                    @error('password')
-                        <div class="alert alert-danger" role="alert">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+                        @error('password')
+                            <div class="alert alert-danger" role="alert">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
                 <button type="submit" class="btn btn-primary">Envoyer</button>
 
