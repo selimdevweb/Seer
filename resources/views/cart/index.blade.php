89470eb7231a769e9c1a4222cc3a59c8668526da
selimdevweb <selim-kune-do@hotmail.fr>
2021-06-01 11:44:53 +0200

    master

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -24,19 +24,16 @@
                         <td scope="row">Colis secs {{ \Carbon\Carbon::parse($billetterie->date)->translatedFormat('d/m/Y') }}</td>
                         <td scope="row">{{ \Cart::get($billetterie->id)->getPriceSum()}} €</td>
                         <td scope="row">
-<<<<<<< HEAD
                             <form action="{{ route('cart.update', $billetterie->id) }}" class="user_shop">
                                 @csrf
                                 <input type="number" name="quantity" value="{{ $billetterie->quantity }}">
                                 <input type="submit" value="Valider">
                             </form>
                         </td>
-=======
+
                             <div class="main__form">
                                 <form action="{{ route('cart.update', $billetterie->id) }}">
                                     @csrf
->>>>>>> 1637e357a82067dc54dc7045284c632c93dff85c
-
                                     <input type="number" name="quantity" value="{{ $billetterie->quantity }}">
                                     <button type="submit">Commander</button>
                                 </form>
