b41b5c877ba1fc6961e0e61f2f6469f9c94c9320
selimdevweb <selim-kune-do@hotmail.fr>
2021-06-10 13:13:16 +0200

    selim

--- resources/views/other/a-propos.blade.php
+++ resources/views/other/a-propos.blade.php
@@ -8,7 +8,7 @@
     @include('user-auth.layouts.seer-nav')
 
     <div class="d-flex justify-content-center">
-        <h1 class="text-secondary">?</h1>
+        <h1 class="text-secondary">SEER?</h1>
         @if (session()->has('message'))
             <div class="w-4/5 m-auto mt-10 pl-2">
                 <p class="w2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl px-4 py-4">
@@ -17,4 +17,22 @@
             </div>
         @endif
     </div>
+
+
+    <div class="card mb-3">
+        <img src="https://wowslider.com/sliders/demo-93/data1/images/lake.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        <img src="https://wowslider.com/sliders/demo-93/data1/images/landscape.jpg" class="card-img-top" alt="...">
+      </div>
 @endsection
--- resources/views/user-auth/layouts/seer-nav.blade.php
+++ resources/views/user-auth/layouts/seer-nav.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav justify-content-center bg-light m-2">
     <li class="nav-item">
-        <a class="nav-link active" href="{{ route('other.a-propos') }}">A propos</a>
+        <a class="nav-link active" href="{{ route('other.a-propos') }}">SEER ?</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('other.team')}}">Team</a>
